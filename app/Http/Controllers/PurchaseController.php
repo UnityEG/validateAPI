9b576000ba8e4e4e852b047ca42493df9c43dd83
Mohamed Atef <en.mohamed.atef@gmail.com>
2015-09-15 22:46:03 +0200

    Add instorePurchase and createPurchasedVoucher methods

--- app/Http/Controllers/PurchaseController.php
+++ app/Http/Controllers/PurchaseController.php
@@ -14,6 +14,7 @@ use App\Http\Requests\PurchaseRequest;
 
 //controllers
 use App\Http\Controllers\ApiController;
+use App\Http\Controllers\VouchersController;
 
 /**
  * Description of PurchaseController
@@ -21,10 +22,48 @@ use App\Http\Controllers\ApiController;
  * @author mohamed
  */
 class PurchaseController extends ApiController{
-//    todo validate customer input credentials
-//    todo OnlinePurchase method
-    public function instorePurchase(PurchaseRequest $request) {
-        dd($request->all());
+    
+    /**
+     * App\Http\Controllers\VouchersController instance
+     * @var object
+     */
+    private $voucher_controller;
+
+
+    public function __construct( VouchersController $voucher_controller) {
+        $this->voucher_controller = $voucher_controller;
     }
-//    todo Create purchased voucher method
+    
+//    todo OnlinePurchase method
+    
+    /**
+     * Purchase vouchers instore
+     * @param PurchaseRequest $request
+     */
+    public function instorePurchase(PurchaseRequest $request) {
+//        todo decisions to make with instorePurchase
+        foreach ( $request->get('data') as $purchased_voucher) {
+            $purchased_voucher_object = $this->createPurchasedVoucher($purchased_voucher);
+//            todo sending virtual voucher according to purchased voucher info
+//            todo prepare reciept for all purchased voucher
+        }//foreach ( $request->get('data') as $purchased_voucher)
+    }
+    
+    /**
+     * Create purchased voucher
+     * @param object $purchased_voucher_object
+     */
+    private function createPurchasedVoucher( $purchased_voucher ) {
+        $purchased_voucher_to_create = [
+          'user_id'=> (int)$purchased_voucher['relations']['user']['data']['user_id']  ,
+            'voucher_parameter_id'=>(int)$purchased_voucher['relations']['voucher_parameter']['data']['voucher_parameter_id'],
+            'value'=>$purchased_voucher['value'],
+            'delivery_date'=>$purchased_voucher['delivery_date'],
+            'recipient_email'=>$purchased_voucher['recipient_email'],
+            'message'=>$purchased_voucher['message']
+        ];
+        $purchased_voucher_object = $this->voucher_controller->store($purchased_voucher_to_create);
+        return $purchased_voucher_object;
+    }
+
 }
