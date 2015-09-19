5124fc599d8f6e5ddfc89f8c0b6b98c3dd7fcf3d
Mohamed Atef <en.mohamed.atef@gmail.com>
2015-09-19 22:50:57 +0200

    Add generateVirtualVoucher method and getDataForEmail method to get virtual voucher for each purchased voucher

--- app/Http/Controllers/PurchaseController.php
+++ app/Http/Controllers/PurchaseController.php
@@ -1,20 +1,12 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 namespace App\Http\Controllers;
 
-//requests
-use Illuminate\Http\Request;
-use App\Http\Requests\PurchaseRequest;
-
-//controllers
+use App\aaa\g;
 use App\Http\Controllers\ApiController;
 use App\Http\Controllers\VouchersController;
+use App\Http\Models\Voucher;
+use App\Http\Requests\PurchaseRequest;
 
 /**
  * Description of PurchaseController
@@ -44,7 +36,7 @@ class PurchaseController extends ApiController{
 //        todo decisions to make with instorePurchase
         foreach ( $request->get('data') as $purchased_voucher) {
             $purchased_voucher_object = $this->createPurchasedVoucher($purchased_voucher);
-//            todo sending virtual voucher according to purchased voucher info
+            $this->generateVirtualVoucher($purchased_voucher_object);
 //            todo prepare receipt for all purchased voucher
         }//foreach ( $request->get('data') as $purchased_voucher)
     }
@@ -64,6 +56,51 @@ class PurchaseController extends ApiController{
         ];
         $purchased_voucher_object = $this->voucher_controller->store($purchased_voucher_to_create);
         return $purchased_voucher_object;
+    }
+    
+    private function generateVirtualVoucher(Voucher $purchased_voucher_object) {
+        
+        // Gathering Data for email
+        $data = $this->getDataForEmail($purchased_voucher_object);
+        
+        // Generate virtual voucher image 
+        $voucher_filename = g::voucher($data);
+        //
+        // Add $voucher_filename to $data array
+        $data['voucher_filename'] = $voucher_filename;
+        //
+        dd($data);
+        return $data;
+    }
+    
+    private function getDataForEmail($gv) {
+        
+        // todo Fix Gathering Data for email
+        $business_logo = '';//todo get business logo_id from business table
+        $m_logo_filename = (is_object($business_logo)) ? 'images/merchant/logos/' . $business_logo->pic . '.' . $business_logo->extension : 'voucher/images/validate_logo.png';
+        // get Gift Vouchers Parameter Terms Of Use
+        $x = '';//todo get use terms related to voucher parameter of the purchased voucher
+        $TermsOfUse = implode(' ● ', $terms);
+        //
+        //
+        $data = array(
+            'm_logo_filename' => $m_logo_filename,
+            'qr_code' => $gv->qr_code,
+            'delivery_date' => $gv->delivery_date,
+            'expiry_date' => $gv->expiry_date,
+            'voucher_value' => $gv->voucher_value,
+            'merchant_business_name' => $gv->parameter->merchant->business_name,
+            'voucher_title' => $gv->parameter->Title,
+            'TermsOfUse' => $TermsOfUse,
+            'merchant_business_address1' => $gv->parameter->merchant->address1,
+            'merchant_business_phone' => $gv->parameter->merchant->phone,
+            'merchant_business_website' => $gv->parameter->merchant->website,
+            'recipient_email' => $gv->recipient_email,
+            'customer_name' => $gv->customer->getName(),
+            'customer_email' => $gv->customer->user->email,
+        );
+        //
+        return $data;
     }
 
 }
