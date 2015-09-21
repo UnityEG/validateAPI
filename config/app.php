2684f02d103912bb27b4944444455bf7ca8d0bae
Mohamed Atef <en.mohamed.atef@gmail.com>
2015-09-21 10:15:46 +0200

    Add IIntervention\Image\ImageServiceProvider and 'Image' => Intervention\Image\Facades\Image::class

--- config/app.php
+++ config/app.php
@@ -137,6 +137,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
+        Intervention\Image\ImageServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -194,6 +195,7 @@ return [
         'View'      => Illuminate\Support\Facades\View::class,
         'JWTAuth'   => Tymon\JWTAuth\Facades\JWTAuth::class,
         'JWTFactory'=> Tymon\JWTAuth\Facades\JWTFactory::class,
+        'Image' => Intervention\Image\Facades\Image::class,
 
     ],
 
