2372dbea19845f37d7b6c87ca578fd3cd22af2e5
Mohamed Atef <en.mohamed.atef@gmail.com>
2015-09-22 21:31:19 +0200

    Create Rule model with its relations methods

---
+++ app/Http/Models/Rule.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Rule extends Model
+{
+    protected $table = "rules";
+    protected $guarded = ['id'];
+    protected $fillable = ['name'];
+    
+    /**
+     * Relationship between Rule Model and UserGroups Model (many to many)
+     * @return object
+     */
+    public function userGroups( ) {
+        return $this->belongsToMany('App\Http\Models\UserGroup', 'user_groups_rules_rel', 'rule_id', 'user_group_id');
+    }
+}
