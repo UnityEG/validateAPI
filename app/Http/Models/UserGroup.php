d21af52c70bc5ff4d25584d71aceac82adbf698f
Mohamed Atef <en.mohamed.atef@gmail.com>
2015-09-22 21:32:30 +0200

    Create UserGroup model with its relations methods

---
+++ app/Http/Models/UserGroup.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserGroup extends Model
+{
+    protected $table = "user_groups";
+    protected $guarded = ['id'];
+    protected $fillable = ['name'];
+    
+    /**
+     * Relationship between UserGroup Model and User Model (many to many)
+     * @return object
+     */
+    public function users( ) {
+        return $this->belongsToMany('App\User', 'users_user_groups_rel', 'user_group_id', 'user_id');
+    }
+    
+    /**
+     * Relationship between Rule Model and UserGroups Model (many to many)
+     * @return object
+     */
+    public function rules( ) {
+        return $this->belongsToMany('App\Http\Models\Rule', 'user_groups_rules_rel', 'user_group_id', 'rule_id');
+    }
+}
