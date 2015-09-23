f6c20f9780a9b85eecea9c1d724e5d505d2682d1
Mohamed Atef <en.mohamed.atef@gmail.com>
2015-09-23 21:47:09 +0200

    Add beforeStandard method to return raw data before applyign Json API standard

--- app/aaa/Transformers/VoucherValidationLogTransformer.php
+++ app/aaa/Transformers/VoucherValidationLogTransformer.php
@@ -7,30 +7,32 @@ use App\aaa\Transformers\Transformer;
 class VoucherValidationLogTransformer extends Transformer {
 
     function transform( $item ) {
-        return [
-                "id" => (string) $item["id"],
-                "value" => (string)$item["value"],
-                "balance" => (string)$item["balance"],
-                "log" => (string)$item["log"],
-                "relations" => [
-                    "voucher"=>[
-                        "data"=>[
-                            "voucher_id" => (string)$item['voucher_id']
-                        ]
-                    ],
-                    "business"=>[
-                        "data"=>[
-                            ["business_id" => (string)$item["business_id"]]
-                        ]
-                    ],
-                    "user"=>[
-                        "data"=>[
-                            ["user_id"=>(string)$item['user_id']]
-                        ]
-                    ]
-    
-                ]
-            ];
+        return ["data"=>  $this->beforeStandard($item)];
     }
-
+    
+    public function beforeStandard( $item) {
+        return [
+            "id"        => ( string ) $item[ "id" ],
+            "value"     => ( string ) $item[ "value" ],
+            "balance"   => ( string ) $item[ "balance" ],
+            "log"       => ( string ) $item[ "log" ],
+            "relations" => [
+                "voucher"  => [
+                    "data" => [
+                        "voucher_id" => ( string ) $item[ 'voucher_id' ]
+                    ]
+                ],
+                "business" => [
+                    "data" => [
+                        ["business_id" => ( string ) $item[ "business_id" ] ]
+                    ]
+                ],
+                "user"     => [
+                    "data" => [
+                        ["user_id" => ( string ) $item[ 'user_id' ] ]
+                    ]
+                ]
+            ]
+        ];
+    }
 }
