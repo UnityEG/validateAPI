20bd7fffeb975dc939dd836d2693438baf3600c0
Mohamed Atef <en.mohamed.atef@gmail.com>
2015-09-24 20:25:34 +0200

    Add response method to customize error response generally to all requests

--- app/Http/Requests/Request.php
+++ app/Http/Requests/Request.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Requests;
 
+use App\Http\Controllers\ApiController;
 use Illuminate\Foundation\Http\FormRequest;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 abstract class Request extends FormRequest
 {
-    //
+    /**
+     * Customize Json Response
+     * @param array $errors
+     * @return JsonResponse
+     */
+    public function response( array $errors ) {
+        if ( $this->ajax() || $this->wantsJson() ) {
+            return (new ApiController() )->setStatusCode( 417 )->respondWithError( 'invalid parameters', $errors );
+        }//if ($this->ajax() || $this->wantsJson())
+
+        return $this->redirector->to( $this->getRedirectUrl() )
+                        ->withInput( $this->except( $this->dontFlash ) )
+                        ->withErrors( $errors, $this->errorBag );
+    }
 }
