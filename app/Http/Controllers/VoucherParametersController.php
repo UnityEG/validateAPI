3f81c919030cc3f42cec133117038b275a9068b7
Mohamed Atef <en.mohamed.atef@gmail.com>
2015-09-26 21:17:43 +0200

    Create store helper method to perform common tasks of storing voucher parameters. Modify storeUpdateHelper method. Create storeValidationHelper to do necessary validations before storing the voucher parameters in the database. Create storeGiftVoucherParameters method to store gift voucher parameters in the database.

--- app/Http/Controllers/VoucherParametersController.php
+++ app/Http/Controllers/VoucherParametersController.php
@@ -7,9 +7,10 @@ use App\aaa\Transformers\VoucherParametersTransformer;
 use App\Http\Controllers\ApiController;
 use App\Http\Models\Business;
 use App\Http\Models\VoucherParameter;
+use App\Http\Requests\Vouchers\VoucherParameters\CreateVoucherParametersRequest;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
-use SebastianBergmann\RecursionContext\Exception;
+use \Exception;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -52,32 +53,25 @@ class VoucherParametersController extends ApiController
     }
 
     /**
-     * Store a newly created resource in storage.
-     *
-     * @param  Request  $request
-     * @return Response
+     * Store Deal voucher parameters
+     * @param CreateVoucherParametersRequest $request
+     * @return Json response
      */
-    public function store( CreateRequest $request ) {
-//        todo create store methods for each type of vouchers
-        $input_before= $request->except('use_terms');
-        
-        $input = $this->storeUpdateHelper($input_before);
-        
-        DB::beginTransaction();
-        if ( !is_array( $input ) ) {
-            return $input;
-        }
-        if($created_voucher_parameters = VoucherParameter::create( $input )){
-        
-        $created_voucher_parameters->useTerms()->attach($request->get('use_terms'));
-        DB::commit();
-            return $created_voucher_parameters;
-        }//if($created_voucher_parameters = VoucherParameter::create( $input ))
-        else{
-            DB::rollBack();
-            return array('error');
-        }
-        
+    public function storeDealVoucherParameters(CreateVoucherParametersRequest $request ) {
+        $raw_input= $request->get("data");
+        $raw_input['voucher_type'] = 'deal';
+        return $this->store($raw_input);
+    }
+    
+    /**
+     * Store Gift voucher patameters
+     * @param CreateVoucherParametersRequest $request
+     * @return Json response
+     */
+    public function storeGiftVoucherParameters( CreateVoucherParametersRequest $request) {
+       $raw_input = $request->get("data") ;
+       $raw_input['voucher_type'] = 'gift';
+       return $this->store($raw_input);
     }
 
     /**
@@ -170,80 +164,97 @@ class VoucherParametersController extends ApiController
 //    Helper Methods
     
     /**
+     * General Store a newly created resource in storage.
+     *
+     * @param  array  $raw_input
+     * @return Response
+     */
+    public function store( array $raw_input ) {
+        $this->storeValidationHelper($raw_input);
+        $input = $this->storeUpdateHelper($raw_input);
+        DB::beginTransaction();
+        if($created_voucher_parameters = VoucherParameter::create( $input )){
+        $created_voucher_parameters->useTerms()->attach($input['use_terms']);
+        DB::commit();
+            $response =  $this->voucherParameterTransformer->transform( $created_voucher_parameters->toArray());
+        }//if($created_voucher_parameters = VoucherParameter::create( $input ))
+        else{
+            DB::rollBack();
+            $response = $this->respondInternalError();
+        }
+        return $response;
+    }
+    
+    /**
      * perform common sanitization for all vouchers
      * @param object $input
      * @return array
      */
-    private function storeUpdateHelper( $input) {
-        if ( isset($input['purchase_start']) && !empty($input['purchase_start'])){
-            $input['purchase_start'] = g::utcDateTime($input['purchase_start'], 'd/m/Y H:i');
+    private function storeUpdateHelper( array $input) {
+        $input['business_id'] = (int)$input['relations']['business']['data']['business_id'];
+        $input['user_id'] = (int)$input['relations']['user']['data']['user_id'];
+        $input['voucher_image_id'] = (int)$input['relations']['voucher_image']['data']['voucher_image_id'];
+        $input['use_terms'] = (array)$input['relations']['use_terms']['data']['use_term_ids'];
+        if ( isset( $input[ 'purchase_start' ] ) && !empty( $input[ 'purchase_start' ] ) ) {
+            $input[ 'purchase_start' ] = g::utcDateTime( $input[ 'purchase_start' ], 'd/m/Y H:i' );
         }//if (isset($input['purchase_start'])&&!empty($input['purchase_start'])){
-        else{
-            $auckland_now_to_utc =  Carbon::now('Pacific/Auckland')->setTimezone('UTC');
-            $input['purchase_start'] = $auckland_now_to_utc;
+        else {
+            $auckland_now_to_utc     = Carbon::now( 'Pacific/Auckland' )->setTimezone( 'UTC' );
+            $input[ 'purchase_start' ] = $auckland_now_to_utc;
         }
-        
-        if ( !empty($input['purchase_expiry']) ) {
-            $input['purchase_expiry'] = g::utcDateTime($input['purchase_expiry'], 'd/m/Y H:i');
+        if ( !empty( $input[ 'purchase_expiry' ] ) ) {
+            $input[ 'purchase_expiry' ] = g::utcDateTime( $input[ 'purchase_expiry' ], 'd/m/Y H:i' );
         }//if ( !empty($input['purchase_expiry']) )
-        
-        $input['is_expire'] = 0;
-        
-        $input['is_display'] = (isset($input['is_display']) && ($input['is_display'] == 1)) ? 1 : 0;
-        
-        $input['is_purchased'] = 0;
-        
-        if ( !empty($input['valid_from']) ) {
-            $input['valid_from'] = g::utcDateTime($input['valid_from'], 'd/m/Y H:i');
+        $input[ 'is_expire' ] = 0;
+        $input[ 'is_display' ] = 1;
+        $input[ 'is_purchased' ] = 0;
+        if ( !empty( $input[ 'valid_from' ] ) ) {
+            $input[ 'valid_from' ] = g::utcDateTime( $input[ 'valid_from' ], 'd/m/Y H:i' );
         }//if ( !empty($input['valid_from']) )
-        
-        if ( !empty($input['valid_until']) ) {
-            $input['valid_until'] = g::utcDateTime($input['valid_until'], 'd/m/Y H:i');
+        if ( !empty( $input[ 'valid_until' ] ) ) {
+            $input[ 'valid_until' ] = g::utcDateTime( $input[ 'valid_until' ], 'd/m/Y H:i' );
         }//if ( !empty($input['valid_until']) )
-        
 //        secure fields from XSS attack
-        $input['short_description'] = (isset($input['short_description']))?preg_replace(['/\<script\>/', '/\<\/script\>/'], '', $input['short_description']):'';
-        
-        $input['long_description'] = (isset($input['long_description']))?preg_replace(['/\<script\>/', '/\<\/script\>/'], '', $input['long_description']):'';
-        
+        $input[ 'short_description' ] = (isset( $input[ 'short_description' ] )) ? preg_replace( ['/\<script\>/', '/\<\/script\>/' ], '', $input[ 'short_description' ] ) : '';
+        $input[ 'long_description' ] = (isset( $input[ 'long_description' ] )) ? preg_replace( ['/\<script\>/', '/\<\/script\>/' ], '', $input[ 'long_description' ] ) : '';
+        return $input;
+    }
+
+    /**
+     * Store Validation helper for important data of each type of vouchers
+     * @param array $input
+     * @return boolean
+     * @throws Exception
+     */
+    private function storeValidationHelper( array $input) {
         switch ( $input['voucher_type']) {
             case 'gift':
                 if ( empty($input['min_value']) || empty($input['max_value']) ) {
-                    $this->setStatusCode(417);
-                    return $this->respondWithError('min_value or max_value incorrect');
+                    throw new Exception('min_value and max_value must exist', 417);
                 }//if ( empty($input['min_value']) || empty($input['max_value']) )
                 break;
-                
             case 'deal':
                 if ( empty($input['retail_value']) || empty($input['value']) ) {
-                    $this->setStatusCode(417);
-                    return $this->respondWithError('retail_value or value incorrect');
+                    throw new Exception('retail_value and value must exist', 417);
                 }//if ( empty($input['retail_value']) || empty($input['value']) )
                 break;
-                
             case 'birthday':
                 if ( empty($input['value'])) {
-                    $this->setStatusCode(417);
-                    return $this->respondWithError('retail_value or value incorrect');
+                    throw new Exception('value must exist', 417);
                 }//if ( empty($input['value']))
                 break;
-                
             case 'discount':
                 if ( empty($input['discount_percentage'])) {
-                    $this->setStatusCode(417);
-                    return $this->respondWithError('retail_value or value incorrect');
+                    throw new Exception('discount_percentage must exist', 417);
                 }//if ( empty($input['value']))
                 break;
-                
             case 'concession':
                 if ( empty($input['retail_value']) || empty($input['value']) ) {
-                    $this->setStatusCode(417);
-                    return $this->respondWithError('retail_value or value incorrect');
+                    throw new Exception('retail_value and value must exist', 417);
                 }//if ( empty($input['retail_value']) || empty($input['value']) )
                 break;
         }//switch ( $input['voucher_type'])
-        
-        return $input;
+        return TRUE;
     }
     
     /**
