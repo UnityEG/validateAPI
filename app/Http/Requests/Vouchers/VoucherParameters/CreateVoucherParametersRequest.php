1fd1cac66186e4024b9a2e04c6b8229429d5e370
Mohamed Atef <en.mohamed.atef@gmail.com>
2015-09-26 21:23:31 +0200

    Modify rules method to suit Json API standards

--- app/Http/Requests/Vouchers/VoucherParameters/CreateVoucherParametersRequest.php
+++ app/Http/Requests/Vouchers/VoucherParameters/CreateVoucherParametersRequest.php
@@ -1,9 +1,8 @@
 <?php
 
-namespace App\Http\Requests\Vouchers\VoucherParameters;
+namespace app\Http\Requests\Vouchers\VoucherParameters;
 
 use App\Http\Requests\Request;
-use Illuminate\Http\JsonResponse;
 
 class CreateVoucherParametersRequest extends Request {
 
@@ -23,31 +22,27 @@ class CreateVoucherParametersRequest extends Request {
      */
     public function rules() {
         $common_rules = [
-            'business_id'           => 'required|integer|exists:business,id',
-            'user_id'               => 'required|integer|exists:users,id',
-            'voucher_image_id'      => 'required|exists:voucher_images,id',
-            'use_terms'             => 'exists:use_terms,id',
-            'voucher_type'          => 'required|string|in:gift,deal,birthday,discount,concession',
-            'title'                 => 'required|string',
-            'purchase_start'        => 'date_format:d/m/Y H:i',
-            'purchase_end'          => 'date_format:d/m/Y H:i',
-            'is_expire'             => 'boolean',
-            'is_display'            => 'boolean',
-            'valid_from'            => 'date_format:d/m/Y',
-            'valid_for_amount'      => 'sometimes|required|integer',
-            'valid_for_units'       => 'sometimes|required|alpha|size:1|in:d,m,y',
-            'valid_until'           => 'date_format:d/m/Y',
-            'quantity'              => 'sometimes|required|regex:/^[19]\d{0,9}$/',
-            'stock_quantity'        => 'integer',
-            'short_description'     => 'string',
-            'long_description'      => 'string',
-            'no_of_uses'            => 'integer',
-            'retail_value'          => 'sometimes|required|numeric',
-            'value'                 => 'sometimes|required|numeric',
-            'min_value'             => 'sometimes|required|numeric',
-            'max_value'             => 'sometimes|required|numeric',
-            'is_valid_during_month' => 'boolean',
-            'discount_percentage'   => 'sometimes|required|numeric'
+            'data.relations.business.data.business_id'           => 'required|integer|exists:business,id',
+            'data.relations.user.data.user_id'                   => 'required|integer|exists:users,id',
+            'data.relations.voucher_image.data.voucher_image_id' => 'required|exists:voucher_images,id',
+            'data.relations.use_terms.data.use_term_ids'         => 'exists:use_terms,id',
+            'data.title'                                         => 'required|string',
+            'data.purchase_start'                                => 'date_format:d/m/Y H:i',
+            'data.purchase_expiry'                               => 'date_format:d/m/Y H:i',
+            'data.valid_from'                                    => 'date_format:d/m/Y H:i',
+            'data.valid_for_amount'                              => 'sometimes|required|integer',
+            'data.valid_for_units'                               => 'sometimes|required|alpha|size:1|in:d,m,y',
+            'data.valid_until'                                   => 'date_format:d/m/Y H:i',
+            'data.quantity'                                      => ['sometimes', 'required', 'integer', 'min:1' ],
+            'data.short_description'                             => 'string',
+            'data.long_description'                              => 'string',
+            'data.no_of_uses'                                    => 'integer',
+            'data.retail_value'                                  => 'sometimes|required|numeric',
+            'data.value'                                         => 'sometimes|required|numeric',
+            'data.min_value'                                     => 'sometimes|required|numeric',
+            'data.max_value'                                     => 'sometimes|required|numeric',
+            'data.is_valid_during_month'                         => 'boolean',
+            'data.discount_percentage'                           => 'sometimes|required|numeric'
         ];
         return $common_rules;
     }
