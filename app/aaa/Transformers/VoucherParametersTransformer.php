27961cf6b7b2ae1793abf7d8fef1edd7067931e9
Mohamed Atef <en.mohamed.atef@gmail.com>
2015-09-26 21:25:42 +0200

    Modify beforeStandard method to deal with non existing data properly

--- app/aaa/Transformers/VoucherParametersTransformer.php
+++ app/aaa/Transformers/VoucherParametersTransformer.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\aaa\Transformers;
+namespace app\aaa\Transformers;
 
 use App\aaa\Transformers\Transformer;
 
@@ -20,22 +20,22 @@ class VoucherParametersTransformer extends Transformer {
             'is_expire'             => ( boolean ) $item[ 'is_expire' ],
             'is_display'            => ( boolean ) $item[ 'is_display' ],
             'is_purchased'          => ( boolean ) $item[ 'is_purchased' ],
-            'valid_from'            => ( string ) $item[ 'valid_from' ],
-            'valid_for_amount'      => ( string ) $item[ 'valid_for_amount' ],
-            'valid_for_units'       => ( string ) $item[ 'valid_for_units' ],
-            'valid_until'           => ( string ) $item[ 'valid_until' ],
+            'valid_from'            => (isset($item['valid_from']))?( string ) $item[ 'valid_from' ]:'',
+            'valid_for_amount'      => (isset($item['valid_for_amount']))?( string ) $item[ 'valid_for_amount' ]:'',
+            'valid_for_units'       => (isset($item['valid_for_units']))?( string ) $item[ 'valid_for_units' ]:'',
+            'valid_until'           => (isset($item['valid_until']))?( string ) $item[ 'valid_until' ]:'',
             'quantity'              => ( string ) $item[ 'quantity' ],
-            'purchased_quantity'    => ( string ) $item[ 'purchased_quantity' ],
-            'stock_quantity'        => ( string ) $item[ 'stock_quantity' ],
+            'purchased_quantity'    => (isset($item['purchased_quantity']))?( string ) $item[ 'purchased_quantity' ]:'',
+            'stock_quantity'        => (isset($item['stock_quantity']))?( string ) $item[ 'stock_quantity' ]:'',
             'short_description'     => ( string ) $item[ 'short_description' ],
             'long_description'      => ( string ) $item[ 'long_description' ],
             'no_of_uses'            => ( string ) $item[ 'no_of_uses' ],
-            'retail_value'          => ( string ) $item[ 'retail_value' ],
-            'value'                 => ( string ) $item[ 'value' ],
-            'min_value'             => ( string ) $item[ 'min_value' ],
-            'max_value'             => ( string ) $item[ 'max_value' ],
-            'is_valid_during_month' => ( boolean ) $item[ 'is_valid_during_month' ],
-            'discount_percentage'   => ( string ) $item[ 'discount_percentage' ],
+            'retail_value'          => (isset($item['retail_value']))?( string ) $item[ 'retail_value' ]:'',
+            'value'                 => (isset($item['value']))?( string ) $item[ 'value' ]:'',
+            'min_value'             => (isset($item['min_value']))?( string ) $item[ 'min_value' ]:'',
+            'max_value'             => (isset($item['max_value']))?( string ) $item[ 'max_value' ]:'',
+            'is_valid_during_month' => (isset($item['is_valid_during_month']))?( boolean ) $item[ 'is_valid_during_month' ]:'',
+            'discount_percentage'   => (isset($item['discount_percentage']))?( string ) $item[ 'discount_percentage' ]:'',
             'created_at'            => ( string ) $item[ 'created_at' ],
             'updated_at'            => ( string ) $item[ 'updated_at' ],
             "relations"             => [
