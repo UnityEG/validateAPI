f8e64b6d003e1c5dee1b41bf8f3ff8ac9e613942
Mohamed Atef <en.mohamed.atef@gmail.com>
2015-09-27 21:31:22 +0200

    - Modify rules method

--- app/Http/Requests/Vouchers/VoucherParameters/UpdateVoucherParametersRequest.php
+++ app/Http/Requests/Vouchers/VoucherParameters/UpdateVoucherParametersRequest.php
@@ -22,13 +22,17 @@ class UpdateVoucherParametersRequest extends Request
      * @return array
      */
     public function rules() {
+//        todo continue refine update voucher rule with relocating properties
+//        todo extend new validation rule for voucher_type_check
+        dd($this->route()->getName());
         $common_rules = [
-            'business_id'           => 'required|integer|exists:business,id',
-            'user_id'               => 'required|integer|exists:users,id',
-            'voucher_image_id'      => 'required|exists:voucher_images,id',
-            'use_terms'             => 'exists:use_terms,id',
-            'voucher_type'          => 'required|string|in:gift,deal,birthday,discount,concession',
-            'title'                 => 'required|string',
+            'data.id'=>'required|integer|min:1|exists:voucher_parameters,id',
+            'business_id'           => 'sometimes|required|integer|exists:business,id',
+            'user_id'               => 'sometimes|required|integer|exists:users,id',
+            'voucher_image_id'      => 'sometimes|required|exists:voucher_images,id',
+            'use_terms'             => 'sometimes|required|exists:use_terms,id',
+//            'voucher_type'          => 'required|string|in:gift,deal,birthday,discount,concession',
+            'title'                 => 'sometimes|required|string',
             'purchase_start'        => 'date_format:d/m/Y H:i',
             'purchase_end'          => 'date_format:d/m/Y H:i',
             'is_expire'             => 'boolean',
@@ -37,8 +41,8 @@ class UpdateVoucherParametersRequest extends Request
             'valid_for_amount'      => 'sometimes|required|integer',
             'valid_for_units'       => 'sometimes|required|alpha|size:1|in:d,m,y',
             'valid_until'           => 'date_format:d/m/Y',
-            'quantity'              => 'sometimes|required|regex:/^[19]\d{0,9}$/',
-            'stock_quantity'        => 'integer',
+            'quantity'              => 'sometimes|required|integer|min:1',
+//            'stock_quantity'        => 'integer',
             'short_description'     => 'string',
             'long_description'      => 'string',
             'no_of_uses'            => 'integer',
@@ -46,7 +50,7 @@ class UpdateVoucherParametersRequest extends Request
             'value'                 => 'sometimes|required|numeric',
             'min_value'             => 'sometimes|required|numeric',
             'max_value'             => 'sometimes|required|numeric',
-            'is_valid_during_month' => 'boolean',
+//            'is_valid_during_month' => 'boolean',
             'discount_percentage'   => 'sometimes|required|numeric'
         ];
         return $common_rules;
