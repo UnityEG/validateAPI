02ba792dc5dc6413b4bbb6111bc597ff59033fab
Mohamed Atef <en.mohamed.atef@gmail.com>
2015-10-02 00:10:24 +0200

    Add beforeStandard method and modify transform method to prepare data for Json API standard

--- app/aaa/Transformers/UserTransformer.php
+++ app/aaa/Transformers/UserTransformer.php
@@ -2,26 +2,65 @@
 
 namespace App\aaa\Transformers;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
- * Description of LessonTransformer
+ * Transform user array to suit standard Json
  *
- * @author Sammy Guergachi <sguergachi at gmail.com>
+ * @author Mohamed Atef <en.mohamed.atef at gmail.com>
  */
 class UserTransformer extends Transformer {
 
-    public function transform($item) {
+    public function transform( $item ) {
         return [
-            'id' => (string)$item['id'],
-            'user_type' => (string)$item['user_type'],
-            'email' => (string)$item['email'],
-            'active' => (boolean) $item['active'],
+            "data"=>  $this->beforeStandard( $item )
         ];
+    }
+
+    public function beforeStandard( $item ) {
+        $response = [
+            "id"               => ( string ) $item[ 'id' ],
+            "facebook_user_id" => ( string ) $item[ 'facebook_user_id' ],
+            "is_active"        => ( boolean ) $item[ 'is_active' ],
+            "email"            => ( string ) $item[ 'email' ],
+            "title"            => ( string ) $item[ 'title' ],
+            "first_name"       => ( string ) $item[ 'first_name' ],
+            "last_name"        => ( string ) $item[ 'last_name' ],
+            "gender"           => ( string ) $item[ 'gender' ],
+            "dob"              => ( string ) $item[ 'dob' ],
+            "address1"         => ( string ) $item[ 'address1' ],
+            "address2"         => ( string ) $item[ 'address2' ],
+            "phone"            => ( string ) $item[ 'phone' ],
+            "mobile"           => ( string ) $item[ 'mobile' ],
+            "is_notify_deal"   => ( boolean ) $item[ 'is_notify_deal' ],
+            "created_at"       => ( string ) $item[ 'created_at' ],
+            "updated_at"       => ( string ) $item[ 'updated_at' ],
+            "relations"        => [
+                "city"     => [
+                    "data" => [
+                        "city_id" => ( string ) $item[ 'city_id' ]
+                    ]
+                ],
+                "region"   => [
+                    "data" => [
+                        "region_id" => ( string ) $item[ 'region_id' ]
+                    ]
+                ],
+                "town"     => [
+                    "data" => [
+                        "town_id" => ( string ) $item[ 'town_id' ]
+                    ]
+                ],
+                "postcode" => [
+                    "data" => [
+                        "postcode_id" => ( string ) $item[ 'postcode_id' ]
+                    ]
+                ]
+            ]
+        ];
+        if ( isset($item['token']) && !empty($item['token']) ) {
+            $response_token['token'] = (string) $item['token'];
+            $response = $response_token+$response;
+        }
+        return $response;
     }
 
 }
