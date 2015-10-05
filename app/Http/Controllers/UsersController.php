230dfa909d0ce071603b17c27c8178fce9fd26be
Mohamed Atef <en.mohamed.atef@gmail.com>
2015-10-06 00:19:13 +0200

    - Correct dependencies. - Add prperties for dependency injection - Add prepareDataForStoringHelper method - Add store method.

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,23 +2,56 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Http\Requests;
-use App\Http\Controllers\Controller;
-use Response;
-use App\User;
+use App\aaa\g;
 use App\aaa\Transformers\UserTransformer;
+use App\Http\Requests\Users\StoreUserRequest;
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Response;
+use App\Http\Models\UserGroup;
 
 class UsersController extends ApiController {
-
+    
+    /**
+     * Instance of UserTransformer class
+     * @var object
+     */
     protected $UserTransformer;
+    
+    /**
+     *Instance of g class
+     * @var object
+     */
+    protected $g;
+    
+    /**
+     *Instance of User Model class
+     * @var object
+     */
+    protected $userModel;
+    
+    /**
+     *Instance of UserGroup Model class
+     * @var object
+     */
+    private $userGroupModel;
 
-    public function __construct(UserTransformer $UserTransformer) {
+    public function __construct(
+            UserTransformer $UserTransformer,
+            g $g,
+            User $user_model,
+            UserGroup $user_group_model
+            ) {
 //        todo exclude store method from jwt.auth 
         // Apply the jwt.auth middleware to all methods in this controller
         $this->middleware('jwt.auth', ['except' => 'store']);
         // 
         $this->UserTransformer = $UserTransformer;
+        $this->g = $g;
+        $this->userModel = $user_model;
+        $this->userGroupModel = $user_group_model;
 //        $this->middleware('auth.basic');
     }
 
@@ -51,8 +84,20 @@ class UsersController extends ApiController {
      * @param  Request  $request
      * @return Response
      */
-    public function store(Request $request) {
-        //todo store new user
+    public function store(StoreUserRequest $request) {
+        $modified_input = $this->prepareDataForStoringHelper($request->json("data"));
+        DB::beginTransaction();
+        $created_user = $this->userModel->create($modified_input);
+        if ( is_object( $created_user ) ) {
+            $customer_group_object = $this->userGroupModel->where('group_name', 'customers')->first();
+            $customer_group_object->users()->attach([$created_user->id]);
+            DB::commit();
+            $response = $this->UserTransformer->transform($created_user->toArray());
+        }else{
+            DB::rollBack();
+            $response = $this->respondInternalError();
+        }//if ( is_object( $created_user ) )
+        return $response;
     }
 
     /**
@@ -93,7 +138,7 @@ class UsersController extends ApiController {
      * @param  int  $id
      * @return Response
      */
-    public function update(Request $request, $id) {
+    public function update(Request $request) {
         //todo update current user
     }
 
@@ -108,6 +153,40 @@ class UsersController extends ApiController {
     }
     
 //    Helpers
-//    todo create prepareDataForStoreHelper
+    
+    /**
+     * Prepare data for storing method
+     * @param array $raw_input
+     * @return array
+     */
+    private function prepareDataForStoringHelper( array $raw_input) {
+        $modified_input['city_id'] = (int)  $this->g->arrayKeySearchRecursively($raw_input, 'city_id');
+        $modified_input['region_id'] = (int)$this->g->arrayKeySearchRecursively($raw_input, 'region_id');
+        $modified_input['town_id'] = (int) $this->g->arrayKeySearchRecursively($raw_input, 'town_id');
+        $modified_input['postcode_id'] = (int) $this->g->arrayKeySearchRecursively($raw_input, 'postcode_id');
+        if ( $is_active = $this->g->arrayKeySearchRecursively( $raw_input, 'is_active') ) {
+            $modified_input['is_active'] = ("false" === $is_active) ? FALSE : TRUE;
+        }else{
+            $modified_input['is_active'] = TRUE;
+        }//if ( $is_active = $this->g->arrayKeySearchRecursively( $raw_input, 'is_active') )
+        $modified_input['email'] = (string)  $this->g->arrayKeySearchRecursively($raw_input, 'email');
+        $modified_input['password'] = Hash::make($this->g->arrayKeySearchRecursively( $raw_input, 'password'));
+        $modified_input['title'] = (string)  $this->g->arrayKeySearchRecursively($raw_input, 'title');
+        $modified_input['first_name'] = (string)  $this->g->arrayKeySearchRecursively($raw_input, 'first_name');
+        $modified_input['last_name'] = (string)  $this->g->arrayKeySearchRecursively($raw_input, 'last_name');
+        ($gender = $this->g->arrayKeySearchRecursively($raw_input, 'gender')) ? $modified_input['gender'] = (string)$gender : FALSE;
+        $modified_input['dob'] = $this->g->utcDateTime($this->g->arrayKeySearchRecursively($raw_input, 'dob').' 00:00', 'm/d/Y H:i');
+        $modified_input['address1'] = (string)$this->g->arrayKeySearchRecursively($raw_input, 'address1');
+        ($address2 = $this->g->arrayKeySearchRecursively($raw_input, 'address2')) ? $modified_input['address2'] = (string)$address2 : FALSE;
+        ($phone = $this->g->arrayKeySearchRecursively($raw_input, 'phone')) ? $modified_input['phone'] = (int)$phone : FALSE;
+        ($mobile = $this->g->arrayKeySearchRecursively($raw_input, 'mobile')) ? $modified_input['mobile'] = (int)$mobile : FALSE;
+        if ( $is_notify_deal = $this->g->arrayKeySearchRecursively( $raw_input, 'is_notify_deal') ) {
+            $modified_input['is_notify_deal'] = ("false" === $is_notify_deal) ? FALSE : TRUE;
+        }else{
+            $modified_input['is_notify_deal'] = TRUE;
+        }//if ( $is_notify_deal = $this->g->arrayKeySearchRecursively( $raw_input, 'is_notify_deal') )
+        return $modified_input;
+    }
+    
 //    todo create prepareDataForUpdateHelper
 }
