a1e6d52d2aa743a754515ef18762e0415c875aa2
Mohamed Atef <en.mohamed.atef@gmail.com>
2015-10-08 21:33:20 +0200

    Create UserGroupTransformer class to get standard Json user group object

---
+++ app/aaa/Transformers/UserGroupTransformer.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\aaa\Transformers;
+
+/**
+ * Description of UserGroupTransformer
+ *
+ * @author mohamed
+ */
+class UserGroupTransformer extends Transformer{
+
+    public function beforeStandard( array $item ) {
+        $response = [
+            "id" => (string)$item['id'],
+            "group_name" => (string)$item['group_name']
+        ];
+        return $response;
+    }
+
+    public function transform( array $item ) {
+        return ["data"=>  $this->beforeStandard($item)];
+    }
+
+}
