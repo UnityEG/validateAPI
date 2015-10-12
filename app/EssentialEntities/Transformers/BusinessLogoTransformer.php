b944595054d650ce10e304ef4e18cf19f95e953c
Mohamed Atef <en.mohamed.atef@gmail.com>
2015-10-12 22:02:18 +0200

    Adjust beforeStandrd method to return with array follow Json API standards

--- app/EssentialEntities/Transformers/BusinessLogoTransformer.php
+++ app/EssentialEntities/Transformers/BusinessLogoTransformer.php
@@ -16,20 +16,18 @@ namespace App\EssentialEntities\Transformers;
 class BusinessLogoTransformer extends Transformer{
 
     public function beforeStandard( array $item ) {
+        $user = (!empty($item['user'])) ? $item['user'] : ["data"=>["user_id"=>(string)$item['user_id']]];
+        $business = (!empty($item['business'])) ? $item['business'] : ["data"=>["business_id"=>(string)$item['business_id']]];
+        $business_logo_link = config('validateconf.default_business_logos_uri').'/'.$item['name'].'.png';
         $response = [
             "id" => (string)$item['id'],
             "name" => (string)$item['name'],
             "relations"=>[
-                "user"=>[
-                    "data" => [
-                        "user_id" => (string)$item['user_id']
-                    ]
-                ],
-                "business" => [
-                    "data" => [
-                        "business_id" => (string)$item['business_id']
-                    ]
-                ]
+                "user"=>$user,
+                "business" => $business
+            ],
+            "links" => [
+                "business_logo_link" => $business_logo_link
             ]
         ];
         return $response;
