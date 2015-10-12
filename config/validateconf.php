930a2577beeecd231c3500dc39cb7c0ce5e12b0a
Mohamed Atef <en.mohamed.atef@gmail.com>
2015-10-12 22:03:06 +0200

    Create configuration file for Validate project

---
+++ config/validateconf.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Default absolute business logos path 
+    |--------------------------------------------------------------------------
+    |
+    | use this path to store and delete business logos.
+    |
+    */
+    'default_business_logos_path' => public_path('images/business/logos/'),
+    
+    /*
+    |--------------------------------------------------------------------------
+    | Default relative business logos path 
+    |--------------------------------------------------------------------------
+    |
+    | use this path to get business logos uri for showing.
+    |
+    */
+    'default_business_logos_uri' => asset('images/business/logos/')
+];
+
