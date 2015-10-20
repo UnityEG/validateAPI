8e24e9a1b03785044ca96427b05090303a650037
Mohamed Atef <en.mohamed.atef@gmail.com>
2015-10-20 15:06:34 +0200

    Apply new rules to rule method in CreateVoucherParameters repques

--- app/Http/Requests/Vouchers/VoucherParameters/CreateVoucherParametersRequest.php
+++ app/Http/Requests/Vouchers/VoucherParameters/CreateVoucherParametersRequest.php
@@ -9,12 +9,11 @@ class CreateVoucherParametersRequest extends Request {
 //todo change class name to be StoreVoucherParametersRequest
     /**
      * Determine if the user is authorized to make this request.
-     *
+     * Only users belong to groups that have rule "voucher_parameter_store" will be authorized
      * @return bool
      */
     public function authorize() {
-//        todo apply authorization rules to restrict storing vocher parameters
-        return true;
+        return ($this->CurrentUserObject->hasRule( 'voucher_parameter_store')) ? TRUE : FALSE;
     }
 
     /**
@@ -25,24 +24,21 @@ class CreateVoucherParametersRequest extends Request {
     public function rules() {
         $common_rules = [
             'data.relations.business.data.business_id'           => 'required|integer|exists:business,id',
-            'data.relations.user.data.user_id'                   => 'required|integer|exists:users,id',
             'data.relations.voucher_image.data.voucher_image_id' => 'required|exists:voucher_images,id',
-//            fix use_term_ids validation rules as an array
-            'data.relations.use_terms.data.use_term_ids'         => 'exists:use_terms,id',
+            'data.relations.use_terms.data.use_term_ids'         => 'required|array',
             'data.title'                                         => 'required|string',
-            'data.purchase_start'                                => 'date_format:d/m/Y H:i',
-            'data.purchase_expiry'                               => 'date_format:d/m/Y H:i',
+            'data.purchase_start'                                => 'required|date_format:d/m/Y H:i|after:today|before:'.$this->request->get( 'data[purchase_expiry]', '', TRUE),
+            'data.purchase_expiry'                               => 'required|date_format:d/m/Y H:i',
             'data.valid_from'                                    => 'date_format:d/m/Y H:i',
             'data.valid_for_amount'                              => 'sometimes|required|integer',
             'data.valid_for_units'                               => 'sometimes|required|alpha|size:1|in:d,m,y',
             'data.valid_until'                                   => 'date_format:d/m/Y H:i',
             'data.quantity'                                      => ['sometimes', 'required', 'integer', 'min:1' ],
-            'data.short_description'                             => 'string',
-            'data.long_description'                              => 'string',
+            'data.short_description'                             => 'required|string',
+            'data.long_description'                              => 'required|string',
             'data.no_of_uses'                                    => 'integer',
         ];
-        $final_rules = array_merge($common_rules, $this->voucherSpecificTypeFieldRules());
-        return $final_rules;
+        return array_merge_recursive($common_rules, $this->voucherSpecificTypeFieldRules(), $this->addUseTermRules( $this->request->get( 'data[relations][use_terms][data][use_term_ids]', [], TRUE)));
     }
     
     public function messages( ) {
@@ -62,8 +58,8 @@ class CreateVoucherParametersRequest extends Request {
         switch ( $route_method_name ) {
             case 'VoucherParameters.storeGiftVoucherParameters':
 //                todo add rule that max_value is must be greater than min_value
-                $voucher_specific_type_rules['data.min_value'] = ['required', 'numeric'];
-                $voucher_specific_type_rules['data.max_value'] = ['required', 'numeric'];
+                $voucher_specific_type_rules['data.min_value'] = ['required', 'numeric', 'min:1', 'max:'.$this->request->get( 'data[max_value]', 0, TRUE)];
+                $voucher_specific_type_rules['data.max_value'] = ['required', 'numeric', 'min:'.$this->request->get('data[min_value]', 0, TRUE)];
                 break;
             case 'VoucherParameters.storeDealVoucherPatameters':
                 $voucher_specific_type_rules['retail_value'] = ['required', 'numeric'];
@@ -72,6 +68,19 @@ class CreateVoucherParametersRequest extends Request {
 //            todo add the rest rules for the rest of voucher types
         }//switch ( $route_method_name )
         return $voucher_specific_type_rules;
+    }
+    
+    /**
+     * Add Rules for use term ids
+     * @param array $use_terms_array
+     * @return array
+     */
+    private function addUseTermRules( array $use_terms_array) {
+        $rules = [];
+        foreach ( $use_terms_array as $key => $value ) {
+            $rules['data.relations.use_terms.data.use_term_ids.'.$key] = ['required', 'integer', 'exists:use_terms,id'];
+        }
+        return $rules;
     }
     
 //    todo create rule to be sure that valid_for_amount is [356 day, 12 month, 48 week]
