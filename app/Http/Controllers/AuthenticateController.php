a88f2a41b107f9800908d129abf22cb0a522a206
Mohamed Atef <en.mohamed.atef@gmail.com>
2015-10-26 20:02:10 +0200

    Modify authenticate method in AuthenticateController class to return with full user data including token

--- app/Http/Controllers/AuthenticateController.php
+++ app/Http/Controllers/AuthenticateController.php
@@ -2,59 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Http\Requests\Users\LoginUserRequest;
 use App\Http\Controllers\ApiController;
 use Tymon\JWTAuth\Facades\JWTAuth;
 use Tymon\JWTAuth\Exceptions\JWTException;
 use Auth;
-use App\User;
-use App\EssentialEntities\Transformers\UserTransformer;
 use App\EssentialEntities\GeneralHelperTools;
-use Exception;
 
 class AuthenticateController extends ApiController {
     
     /**
-     *instance of UserTransformer class
-     * @var object
-     */
-    private $userTransformer;
-    
-    /**
-     *instance of JWTAuth class
-     * @var object
-     */
-    private $JWTAuth;
-
-
-    public function __construct(
-    UserTransformer $user_transformer,
-            JWTAuth $jwtauth
-            ) {
-        // Apply the jwt.auth middleware to all methods in this controller
-        // except for the authenticate method. We don't want to prevent
-        // the user from retrieving their token if they don't already have it
-        $this->middleware('jwt.auth', ['except' => ['authenticate']]);
-        $this->userTransformer = $user_transformer;
-        $this->JWTAuth = $jwtauth;
-    }
-
-    /**
-     * Display a listing of the resource.
-     *
+     * Login user and return with user information with token or error
+     * @param \App\Http\Requests\Users\LoginUserRequest $request
+     * @param \App\EssentialEntities\GeneralHelperTools $general_helper_tools
      * @return Response
-     */
-    public function index() {
-        // Retrieve all the users in the database and return them
-//        $users = User::all();
-//        return $users;
-    }
-    
-    /**
-     * 
-     * @param Request $request
-     * @return type
      */
     public function authenticate(  LoginUserRequest $request, GeneralHelperTools $general_helper_tools) {
         $raw_input = $request->json("data");
@@ -63,11 +24,10 @@ class AuthenticateController extends ApiController {
         // verify the credentials and create a token for the user
         if (!$token = JWTAuth::attempt($credentials)) {
             throw new JWTException('invalid credentials', 417);
-        }
-        $user = Auth::user()->toArray();
-        $user['token'] = $token;
-        // if no errors are encountered we can return a JWT
-        return $this->userTransformer->transform($user);
+        }//if (!$token = JWTAuth::attempt($credentials))
+        $user = Auth::user()->getStandardJsonFormat();
+        $user["data"]["token"] = $token;
+        return $user;
     }
     
     /**
@@ -75,8 +35,9 @@ class AuthenticateController extends ApiController {
      * @return Json response
      */
     public function logout( ) {
+        $this->middleware('jwt.auth');
         if(JWTAuth::invalidate(JWTAuth::getToken())){
-            return $this->respondWithError('token became invalid');
-        }
+            return $this->respond('token became invalid');
+        }//if(JWTAuth::invalidate(JWTAuth::getToken()))
     }
 }
