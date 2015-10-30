47881a892dd3b9e76c5ee1c032bc9e1db440e944
Mohamed Atef <en.mohamed.atef@gmail.com>
2015-10-30 18:37:43 +0200

    Modify store method and Create generateOrderNumber method into OrdersController class

--- app/Http/Controllers/PurchaseControllers/OrdersController.php
+++ app/Http/Controllers/PurchaseControllers/OrdersController.php
@@ -11,19 +11,29 @@ class OrdersController extends ApiController
 {
     /**
      * Store new Order
+     * tax will come with every order to avoid the problem of changing taxes and old order calculations error
+     * @param double $tax tax to be added
+     * @return \App\Http\Models\Order Created Order
      */
-    public function store(){
+    public function store($tax=0){
         $order_info_array = [
             'user_id' => (int)JWTAuth::parseToken()->authenticate()->id,
-//            todo create method to generate order number
-            'number' => 12345,
-//            todo find out where to get tax
-            'tax' => ''
+            'number' => $this->generateOrderNumber(),
+            'tax' => $tax
         ];
         $created_order_object = Order::create($order_info_array);
         if ( is_object($created_order_object) ) {
             return $created_order_object;
-        }
+        }//if ( is_object($created_order_object) )
         throw new Exception('Failed to create new order', 500);
+    }
+    
+    /**
+     * Generate Order number the largest order number found in database + 1 starting from 1001
+     * @return integer
+     */
+    private function generateOrderNumber(){
+        $largest_order_number = max(array_map('intval', Order::lists('number')->toArray()));
+        return (1000 > $largest_order_number) ? 1001 : ++$largest_order_number;
     }
 }
